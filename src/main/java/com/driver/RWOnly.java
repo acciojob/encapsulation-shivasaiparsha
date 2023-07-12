6df707b8aec40f3facd4fa3b0df262161ea985e6
shivasaiparsha <112009608+shivasaiparsha@users.noreply.github.com>
2023-07-12 07:00:14 +0530

    encapsulaton

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,19 @@
 package com.driver;
-
+import java.util.*;
 public class RWOnly {
-    
+
+    private String member;
+
+    RWOnly(String member)
+    {
+        this.member=member;
+    }
+
+    public String getMember() {
+        return member;
+    }
+//
+    public void setMember(String member) {
+        this.member = member;
+    }
 }
